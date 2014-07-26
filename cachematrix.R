918885e9c80aa1ac9e4e6c845c045eb91d73f1ca
basamson <brian@samsonprojectservices.com>
2014-07-26 11:58:28 -0400

    Fulfilled programming assignment 2 requirements. Sample usage of a 6x6 matrix:
    > source("cacheMatrix.R")
    > a <- makeCacheMatrix(matrix(c(1, 1,  1,  1,  1, 1,
    +                               1, 2,  3,  4,  5, 6,
    +                               1, 3,  6, 10, 15, 21,
    +                               1, 4, 10, 20, 35, 56,
    +                               1, 5, 15, 35, 70, 126,
    +                               1, 6, 21, 56, 126, 252),ncol=6,byrow=T))
    > cacheSolve(a)
         [,1] [,2] [,3] [,4] [,5] [,6]
    [1,]    6  -15   20  -15    6   -1
    [2,]  -15   55  -85   69  -29    5
    [3,]   20  -85  146 -127   56  -10
    [4,]  -15   69 -127  117  -54   10
    [5,]    6  -29   56  -54   26   -5
    [6,]   -1    5  -10   10   -5    1
    > cacheSolve(a)
    getting cached data
         [,1] [,2] [,3] [,4] [,5] [,6]
    [1,]    6  -15   20  -15    6   -1
    [2,]  -15   55  -85   69  -29    5
    [3,]   20  -85  146 -127   56  -10
    [4,]  -15   69 -127  117  -54   10
    [5,]    6  -29   56  -54   26   -5
    [6,]   -1    5  -10   10   -5    1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This set of functions, when provided an invertable matrix, will 
+# return the inverse of that matrix, and cache its results for future
+# reference.  If the inverse of a previously-supplied matrix is supplied, 
+# the function will return its cached inverse result rather than consuming
+# computing power to perform a recalculation.
+#
+# Full 2x2 Example:
+# > source("cacheMatrix.R")
+# > a <-makeCacheMatrix(matrix(c(4,3,3,2),ncol=2,byrow=T)
+# > cacheSolve(a)
+# [,1] [,2]
+# [1,]   -2    3
+# [2,]    3   -4
+# > cacheSolve(a)
+# getting cached data
+# [,1] [,2]
+# [1,]   -2    3
+# [2,]    3   -4
 
-## Write a short comment describing this function
+
+#   Purpose: Create a special matrix that can cache its inverse
+#     Usage: makeCacheMatrix(x)
+# Arguments: x         a matrix object
+#   Outputs: none
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinversematrix <- function(solve) m <<- solve
+      getinversematrix <- function() m
+      list(set = set, get = get,
+           setinversematrix = setinversematrix,
+           getinversematrix = getinversematrix)
 }
 
 
-## Write a short comment describing this function
+#   Purpose: computes the inverse of the special "matrix" returned 
+#            by `makeCacheMatrix` above. If the inverse has
+#            already been calculated (and the matrix has not changed), then
+#            `cacheSolve` should retrieve the inverse from the cache
+#     Usage: cacheSolve(x, ...)
+# Arguments: x         a makeCacheMatrix object
+#   Outputs: a inverse matrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinversematrix()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinversematrix(m)
+      m
 }
